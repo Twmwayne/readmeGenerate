38ff4320596942287e57a0787634ed1199b9d85c
Travis Mendenhall <twmwayne@gmail.com>
2020-11-12 13:33:06 -0700

    made questions for index.js

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
 // array of questions for user
 const questions = [
+    {
+        name: "username",
+        message: "What is your GitHub username?",
+        validate: verifyGitHubAccount
+    },
+    {
+        type: "list",
+        name: "repoName",
+        message: "Select the project repo:",
+        filter:setRepoDefaults
+    },
+    {
+        name: "title",
+        message: "Enter a project title:",
+    },
+    {
+        name: "description",
+        message: "Enter a project description:",
+    },
+    {
+        name: "installation",
+        message: "Enter installation instructions:"
+    },
+    {
+        name: "usage",
+        message: "Enter usage directions:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Select license type:",
+        choices: ["copyleft","lpgl","MIT","permissive","proprietary","public"]
+    },
+    {
+        name: "contributors",
+        message: "Enter contibutors:"
+    },
+    {
+        name: "tests",
+        message: "Enter tests:"
+    },
+    {
+        name: "email",
+        message: "Enter contact email:",
+        validate: validateEmail
+    }
 
 ];
 
